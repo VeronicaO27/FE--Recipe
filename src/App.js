edfa420ff97aedac4c2144b11f904400908d3ff6
Veronica Osei <veronicaosei273@gmail.com>
2023-02-08 21:55:17 -0600

    Working FE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import Recipe from "./RecipeCard";
 
-function App() {
+export default function App() {
+  // const APP_KEY = "2853e9ccf22f104dab4034e2f9766207";
+  // const APP_ID = "6957ebee";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("kale");
+  
+  const url = `http://localhost:8080/recipes?query=${query}`;
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="hero">
+        <p
+          style={{
+            textAlign: "center",
+            color: "#222",
+            opacity: "0.8",
+            fontSize: "12px",
+            padding: "0px",
+            margin: "0px",
+            lineHeight: "18px"
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          we're limited to <b>5 API requests a minute</b>
+        </p>
+        <div>
+          <h1 className="hero-title">Soul Food Recipes</h1>
+          <p className="subtitle">
+            Search for recipes, type a dish or an ingredient
+          </p>
+          <p className="Superscript">Good Food always warms up the soul...</p>
+        </div>
+
+        <form onSubmit={getSearch} className="search-form">
+          <input
+            className="search-bar"
+            value={search}
+            onChange={updateSearch}
+          />
+          <button className="search-btn" type="submit">
+            search
+          </button>
+        </form>
+      </div>
+      <div className="wrapper">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.calories}
+            title={recipe.recipe.label}
+            cal={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+            time={recipe.recipe.totalTime}
+            // source={recipe.recipe.url}
+            servings={recipe.recipe.yeild}
+          />
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
